e3c7fefd62e769406740f389b0fb9ad73404da2f
Pascâl Wittkowski <p.wittkowski@stud.hs-wismar.de>
2020-12-17 16:03:31 +0100

    Projektdateien hinzufügen.

---
+++ NodejsWebApp2/components/app.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+//This is gonna be my App
+
+const App = () => {
+    return {
+        <h1>This is my App</h1>
+    }
+}
---
+++ Script1.js
@@ -0,0 +1,8 @@
+// import React
+import React from 'react';
+
+//myApp
+
+const App = () => {
+    <h1>Moin ich bims</h1>
+}
